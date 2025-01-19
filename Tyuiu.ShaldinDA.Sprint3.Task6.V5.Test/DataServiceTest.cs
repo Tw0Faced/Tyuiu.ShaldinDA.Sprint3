bc5bf5c08ff15673927ec3a747a2f7abff4f1c51
Данил Шалдин <steampanker45@gmail.com>
2025-01-19 14:50:19 +0500

    Task 6

---
+++ Tyuiu.ShaldinDA.Sprint3.Task6.V5.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.ShaldinDA.Sprint3.Task6.V5.Lib;
+
+namespace Tyuiu.ShaldinDA.Sprint3.Task6.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int startv = 15;
+            int stopv = 22;
+
+            int res = ds.GetSumTheDivisors(startv, stopv);
+
+            int wait = 242;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ShaldinDA.Sprint3.Task6.V5/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.ShaldinDA.Sprint3.Task6.V5.Lib;
+namespace Tyuiu.ShaldinDA.Sprint3.Task6.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Шалдин Данил Андреевич | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* #3.6 Обработка целочисленной информации                                 *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #5                                                            *");
+            Console.WriteLine("* Выполнил: Шалдин Данил Андреевич | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Напишите программу, которая ищет среди целых чисел, принадлежащих числово*");
+            Console.WriteLine("*ому отрезку [15, 22] сумму всех делителей                                *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startv = 15;
+            int stopv = 22;
+            Console.WriteLine(startv);
+            Console.WriteLine(stopv);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.GetSumTheDivisors(startv, stopv));
+            Console.ReadKey();
+        }
+    }
+}
